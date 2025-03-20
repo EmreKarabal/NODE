d5a28bf9f41cbbb5e4fffc34e6174ee4675265a9
EmreKarabal <emrekarabal61@gmail.com>
2025-03-20 10:10:40 +0300

    after ep23

--- api/app.js
+++ api/app.js
@@ -18,7 +18,7 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-
+console.log(process.env)
 
 app.use('/api', require('./routes/index'));
 
